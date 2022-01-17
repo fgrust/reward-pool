e0cde8f8eef9707eae1d23740c2d1cd39c4514dc
Freezy Gem <freezy.gem0918@outlook.com>
2022-01-17 11:25:41 -0800

    chore: create process routes

--- src/processor.rs
+++ src/processor.rs
@@ -1,5 +1,53 @@
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 
-pub fn process(_program_id: &Pubkey, _accounts: &[AccountInfo], _input: &[u8]) -> ProgramResult {
+use crate::instruction::{InitData, InstructionType, StakeData};
+
+pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], input: &[u8]) -> ProgramResult {
+    let instruction = InstructionType::unpack(input)?;
+
+    match instruction {
+        InstructionType::CreatePool(init_data) => {
+            process_create_stake_pool(program_id, accounts, init_data)
+        }
+        InstructionType::CreateStakeUser => process_create_stake_user(program_id, accounts),
+        InstructionType::Stake(StakeData { amount }) => process_stake(program_id, accounts, amount),
+        InstructionType::Unstake(StakeData { amount }) => {
+            process_unstake(program_id, accounts, amount)
+        }
+
+        InstructionType::Claim => process_claim(program_id, accounts),
+        InstructionType::Refresh => process_refresh(program_id, accounts),
+    }
+}
+
+pub fn process_create_stake_pool(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    init_data: InitData,
+) -> ProgramResult {
+    Ok(())
+}
+
+pub fn process_create_stake_user(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    Ok(())
+}
+
+pub fn process_stake(program_id: &Pubkey, accounts: &[AccountInfo], amount: u64) -> ProgramResult {
+    Ok(())
+}
+
+pub fn process_unstake(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    amount: u64,
+) -> ProgramResult {
+    Ok(())
+}
+
+pub fn process_claim(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    Ok(())
+}
+
+pub fn process_refresh(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     Ok(())
 }
